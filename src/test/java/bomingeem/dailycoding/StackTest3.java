3a5bcf2accdb9739de5ea3a575e37e6d93ab6e90
bmkim <bomingeem@gmail.com>
2021-11-01 23:14:02 +0900

    pushed null value test on stack

--- src/test/java/bomingeem/dailycoding/StackTest3.java
+++ src/test/java/bomingeem/dailycoding/StackTest3.java
@@ -1,9 +1,33 @@
 package bomingeem.dailycoding;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class StackTest3 {
     private Stack stack;
 
+    @BeforeEach
+    public void init() {
+        stack = new Stack();
+    }
+
+    @Test
+    public void pushedNullValueTest() {
+        stack.push(null);
+        assertThat(stack.isEmpty(), is(false));
+    }
+
     public static class Stack {
-        public Stack() {}
+        private Integer data;
+        public Stack() {};
+
+        public void push(Integer data) {
+        }
+
+        public boolean isEmpty() {
+            return true;
+        }
     }
 }
