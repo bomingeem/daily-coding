965451688a8d77d793f50145a596b0723deba38c
bmkim <bomingeem@gmail.com>
2021-11-06 11:40:40 +0900

    check for null value on the stack

--- src/test/java/bomingeem/dailycoding/StackTest3.java
+++ src/test/java/bomingeem/dailycoding/StackTest3.java
@@ -1,9 +1,11 @@
 package bomingeem.dailycoding;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StackTest3 {
     private Stack stack;
@@ -16,7 +18,9 @@ public class StackTest3 {
     @Test
     public void pushedNullValueTest() {
         stack.push(null);
-        assertThat(stack.isEmpty(), is(false));
+        assertThrows(NullPointerException.class, () -> {
+            System.out.println("null cannot be allowed");
+        });
     }
 
     public static class Stack<Integer> {
@@ -29,6 +33,10 @@ public class StackTest3 {
         };
 
         public Integer push(Integer data) {
+            //TODO: NullPointerException 에러 시 어떻게 에러를 처리해야 올바른 방법일까?
+            if (data == null) {
+                throw new NullPointerException("null cannot be allowed");
+            }
             array[0] = data;
             return data;
         }
