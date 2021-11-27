8e7e6a8ad7b55dbe866baecbc853213082d77e51
bmkim <bomingeem@gmail.com>
2021-11-27 18:46:03 +0900

    leaf year method initialization

---
+++ src/test/java/bomingeem/dailycoding/LeafYearTest.java
@@ -0,0 +1,18 @@
+package bomingeem.dailycoding;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class LeafYearTest {
+    private LeafYear leafYear;
+
+    @BeforeEach
+    public void init() {
+        leafYear = new LeafYear();
+    }
+
+    public static class LeafYear {
+        public static void leafYear(int year) {
+
+        }
+    }
+}
