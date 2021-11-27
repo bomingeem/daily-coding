553cb4dcfb3fa06f33057a907f8ae830fe5ec724
bmkim <bomingeem@gmail.com>
2021-11-27 18:55:23 +0900

    check multiple of four test method

--- src/test/java/bomingeem/dailycoding/LeafYearTest.java
+++ src/test/java/bomingeem/dailycoding/LeafYearTest.java
@@ -1,8 +1,13 @@
 package bomingeem.dailycoding;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class LeafYearTest {
+    //윤년은 연도가 4의 배수이면서 100의 배수가 아닐 때 또는 400의 배수일 때이다.
     private LeafYear leafYear;
 
     @BeforeEach
@@ -10,9 +15,25 @@ public class LeafYearTest {
         leafYear = new LeafYear();
     }
 
-    public static class LeafYear {
-        public static void leafYear(int year) {
+    @Test
+    public void checkMultipleOfFourTest() {
+        //given
+        int year = 2012;
 
+        //when
+        int result = leafYear.leafYear(year);
+
+        //then
+        assertThat(result).isEqualTo(1);
+    }
+
+    public static class LeafYear {
+        public static int leafYear(int year) {
+            if (year%4 == 0) {
+                return 1;
+            } else {
+                return 0;
+            }
         }
     }
 }
